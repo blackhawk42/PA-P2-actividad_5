b84189119be008dc083f6791b9921e0cfad68e30
Daniel Chávez <danielsaul96@gmail.com>
2019-03-25 20:55:55 -0600

    Completed exercise 03

--- 03.c
+++ 03.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
-#include <process.h>
-#include <time.h>
-
-void sleep(int s) {
-    time_t now = time(NULL);
-    time_t goal = now + s;
-
-    while( (now = time(NULL)) < goal ){}
-    
-}
+#include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
-    
+	int seconds = 5;
+
+	printf("Ready to fork...\n");
+	pid_t pid = fork();
+	if (pid < 0) {
+		fprintf(stderr, "Error while forking\n");
+		exit(1);
+	}
+	printf("Forked!\n");
+
+	if (pid != 0) {
+		printf("Father waiting for child (PID: %d) to finish\n", pid);
+
+		wait(NULL);
+
+		printf("Father finished waiting, child exited.\n");
+		printf("Now I'll list some files. See ya!\n");
+
+		execlp("ls", "ls", "-l", NULL);
+	}
+	else {
+		printf("Child (PID: %d) will sleep for %d seconds\n", getpid(), seconds);
+		sleep(seconds);
+		exit(0);
+	}
+
+	printf("This should not execute, as the exec repaces the father process and the child already exits\n");    
+	return 0;
 }
