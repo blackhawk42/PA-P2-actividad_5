1a4be720f1902f4c22efdc0703f4eb150a9fe741
Daniel Chávez <danielsaul96@gmail.com>
2019-03-25 18:47:23 -0600

    Initial commit

---
+++ 01.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <process.h>
+
+void trim_newline(char *str) {
+    for(char *ptr = str; *ptr != '\0'; ptr++) {
+        if (*ptr == '\n') {
+            *ptr = '\0';
+            break;
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    char command[1024];
+    char arg1[1024];
+    char arg2[1024];
+    char *args[4] = {command, arg1, arg2, NULL};
+
+    printf("Give me a command: ");
+    fgets(command, 1024, stdin);
+    trim_newline(command);
+
+    printf("Give a command line argument: ");
+    fgets(arg1, 1024, stdin);
+    trim_newline(arg1);
+
+    printf("Give me a second command line argument: ");
+    fgets(arg2, 1024, stdin);
+    trim_newline(arg2);
+
+    
+    execvp(args[0], (const char * const*)args);
+
+    return 0;
+}
---
+++ 02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <process.h>
+
+int main(int argc, char const *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Invoke with the name of the dir you want to backup\n");
+        exit(2);
+    }
+
+    char const *backup_dir = "bckup";
+
+    execlp("cp", "cp", "-r", argv[1], backup_dir, NULL );
+    
+    return 0;
+}
